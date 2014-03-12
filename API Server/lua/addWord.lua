23fe7a4e6796c0ed1f5361fd8c261b39e46cceea
notcome <notcome@me.com>
2014-03-12 16:56:22 +0800

    Lua scripts debugged

--- API Server/lua/addWord.lua
+++ API Server/lua/addWord.lua
@@ -4,7 +4,7 @@ if redis.call('EXISTS', KEYS[1]) == 1 then
   return false
 end
 
-redis.call('SET', KEYS[1], ARGV[1]),
+redis.call('SET', KEYS[1], ARGV[1])
 redis.call('INCR', KEYS[2])
 
 return true
--- API Server/lua/updateQuiz.lua
+++ API Server/lua/updateQuiz.lua
@@ -7,7 +7,7 @@ else
   intervals = {5, 15, 60, 180, 720, 2880, 5760, 11520, 23040}
 end
 
-local star = redis.call('GET', KEYS[2])
+local star = tonumber(redis.call('GET', KEYS[2]))
 local time = ARGV[1] + intervals[star] * (1 + ARGV[3])
 
 return redis.call('ZADD', KEYS[1], time, ARGV[2])
