13df287afe3be2bf8ff9a7358f7d468fcad374e7
notcome <notcome@me.com>
2014-03-13 11:37:10 +0800

    verify module updated

--- API Server/router/signature.js
+++ API Server/router/signature.js
@@ -1,39 +1,9 @@
 var crypto = require('crypto');
-var querystring = require('querystring');
+var qs = require('querystring');
 
-var TLT;
-
-function textToSignature (content, key) {
+module.exports = function (method, path, query, key) {
+  query = qs.stringify(query);
   var hmac = crypto.createHmac('sha1', key);
-  hmac.update(content);
-  return new Buffer(hmac.digest()).toString('base64');
-}
-
-function generateSignature (method, path, query, key) {
-  query = querystring.stringify(query.SortedObject());
-  return textToSignature([method, path, query].join('\n'), key);
-}
-
-function timestampExpired (timestamp) {
-  timestamp = parseInt(timestamp);
-  var now = new Date().getTime();
-  if (timestamp > now) return true;
-  if (now - timestamp > TLT * 1000) return true;
-  return false;
-}
-
-function verifySignature (req, key) {
-  var method = req.method,
-    pathname = req.pathname,
-       query = new Object(req.query);
-
-  if (timestampExpired(query.timestamp)) return false;
-  var signature = query.signature;
-  delete query.signature;
-  return signature == generateSignature(method, pathname, query, key);
-}
-
-module.exports = function (auth_tlt) {
-  if (auth_tlt) TLT = auth_tlt;
-  return verifySignature;
+  hmac.update([method, path, query].join('\n'));
+  return hmac.digest('base64');
 }
---
+++ API Server/router/verify.js
@@ -0,0 +1,44 @@
+var config, redis
+
+function timestampExpired (timestamp) {
+  timestamp = parseInt(timestamp);
+  var now = new Date().getTime();
+  if (timestamp > now) return true;
+  if (now - timestamp > config.auth.auth_tlt * 1000) return true;
+  return false;
+}
+
+function getPassword (username, cb) {
+  redis.get([config.prefix, 'auth', username].join('.'), cb);
+}
+
+function checkParameters (req, cb) {
+  if (timestampExpired(req.query.timestamp))
+    cb(null, config.errors.auth.timestamp_expired);
+  else getPassword(req.query.username, function (err, key) {
+    if (err) cb(err);
+    else if (key == null)
+      cb(null, config.errors.auth.user_not_existed);
+    else verifySignature(req, key, cb);
+  });
+}
+
+function verifySignature (req, key, cb) {
+  var client_side_result = req.query.signature, 
+      server_side_result;
+  delete req.query.signature;
+
+  if (req.query.version != '1') {
+    cb(null, config.errors.auth.auth_version_not_supported);
+    return;
+  } else {
+    server_side_result = require('./signature')(req.method, req.pathname, req.query, key);
+    if (client_side_result == server_side_result) cb();
+    else cb(null, config.errors.auth.auth_failed);
+  }
+}
+
+module.exports = function (_config, _redis) {
+  config = _config, redis = _redis;
+  return checkParameters;
+}
